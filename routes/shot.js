c2bc301427c9f7611dc14427678d400f7acc7b46
Marvin Zwolsman <marvin.zwolsman@gmail.com>
2017-05-17 13:15:34 +0200

    decoupled echoing of the image to a seperate function, made download an option

--- routes/shot.js
+++ routes/shot.js
@@ -10,7 +10,6 @@ const router = new koaRouter()
 //db
 const db = require('../db')
 
-//debug
 //Get information
 router.get('/i(nfo|nformation)?/:sid([a-zA-Z0-9-_]{7,14}):ext(.[jpg|png|gif|webp|tif|bmp|jxr]+)?', async(ctx, next) => {
     let exists = await db.existsAsync(ctx.params.sid)
@@ -38,25 +37,43 @@ router.get('/i(nfo|nformation)?/:sid([a-zA-Z0-9-_]{7,14}):ext(.[jpg|png|gif|webp
     ctx.body = result
 })
 
+router.get('/d(ownload)?/:sid([a-zA-Z0-9-_]{7,14}):ext(.[jpg|png|gif|webp|tif|bmp|jxr]+)?', async (ctx, next) => {
+    let exists = await db.existsAsync(ctx.params.sid)
 
-router.get('/:id', async(ctx, next) => {
-    let exists = await db.existsAsync(ctx.params.id)
+    if (!exists) {
+        ctx.throw(404)
+        return
+    }
+    let shot = await db.hgetallAsync(ctx.params.sid)
+
+    ctx.response.attachment(ctx.params.sid + shot.ext)
+    await echoShot(ctx.params.sid, ctx, shot)
+})
+
+
+router.get('/:sid', async(ctx, next) => {
+    let exists = await db.existsAsync(ctx.params.sid)
 
     if (!exists) {
         ctx.throw(404)
         return
     }
 
-    let data = await db.hgetallAsync(ctx.params.id)
+    await echoShot(ctx.params.sid, ctx)
 
-    ctx.response.set('Content-Type', data.mime)
-    ctx.response.set('Content-Length', data.size)
-    ctx.body = fs.createReadStream('/var/lensert/' + ctx.params.id + data.ext)
-
-    db.saddAsync(ctx.params.id + ':views', ctx.request.ip).then(result => {
+    db.saddAsync(ctx.params.sid + ':views', ctx.request.ip).then(result => {
         db.incrby('views', result)
         if(result > 0)
-            debug('new view; id: ' + chalk.yellow(ctx.params.id) + ', ip: ' + chalk.yellow(ctx.request.ip))
+            debug('new view; id: ' + chalk.yellow(ctx.params.sid) + ', ip: ' + chalk.yellow(ctx.request.ip))
     })
 })
+
+async function echoShot(id, ctx, shot) {
+    if(shot === undefined)
+        shot = await db.hgetallAsync(id)
+
+    ctx.response.set('Content-Type', shot.mime)
+    ctx.response.set('Content-Length', shot.size)
+    ctx.body = fs.createReadStream('/var/lensert/' + id + shot.ext)
+}
 module.exports = router
