219a6e0dcc789608588ec4b9db686337a27d0d47
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2017-05-17 20:26:39 +0200

    Updated db, added error message when redis can't be reached. Added package description + repo

--- db.js
+++ db.js
@@ -1,10 +1,18 @@
 const redis = require('redis')
 const debug = require('debug')('lensert-server:redis')
 const bluebird = require('bluebird')
+const chalk = require('chalk')
 
 const client = redis.createClient()
 client.on('connect', function() {
     debug('connected to redis')
+})
+
+client.on('error', function(err) {
+    if(err.code == 'ECONNREFUSED') {
+        debug('Redis server not running on ' + chalk.yellow(err.address + ":" + err.port))
+        process.exit(1);
+    }
 })
 
 module.exports = bluebird.promisifyAll(client)
