ab66ac46a70977d99cd3dffead928b859d053065
Marvin Zwolsman <marvin.zwolsman@gmail.com>
2017-05-17 12:43:49 +0200

    viewing image will increase view count of the image

--- routes/shot.js
+++ routes/shot.js
@@ -2,6 +2,7 @@
 const koaRouter = require('koa-router')
 const debug = require('debug')('lensert-server:upload')
 const chalk = require('chalk')
+const fs = require('fs')
 
 //router
 const router = new koaRouter()
@@ -33,8 +34,26 @@ router.get('/i(nfo|nformation)?/:sid([a-zA-Z0-9-_]{7,14}):ext(.[jpg|png|gif|webp
         views: views
     })
 
-    //smembers, hgetall
     ctx.body = result
 })
 
+
+router.get('/:id', async(ctx, next) => {
+    let exists = await db.existsAsync(ctx.params.id)
+
+    if (!exists) {
+        ctx.throw(404)
+        return
+    }
+
+    let data = await db.hgetallAsync(ctx.params.id)
+
+    ctx.response.set('Content-Type', data.mime)
+    ctx.response.set('Content-Length', data.size)
+    ctx.body = fs.createReadStream('/var/lensert/' + ctx.params.id + data.ext)
+
+    db.saddAsync(ctx.params.id + ':views', ctx.request.ip).then(result => {
+        db.incrby('views', result)
+    })
+})
 module.exports = router
