2934c97f0f5dc583844f0ebefe8f275b71520e75
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2017-05-20 17:44:14 +0200

    added https server exporting

--- server.js
+++ server.js
@@ -2,16 +2,28 @@ const fs = require('fs')
 const path = require('path')
 const app = require('./app')
 
-let server = null
-if (fs.existsSync('/etc/letsencrypt/live/www.lensert.com/')) {
-    let options = {
-        key: fs.readFileSync('/etc/letsencrypt/live/www.lensert.com/privkey.pem'),
-        cert: fs.readFileSync('/etc/letsencrypt/live/www.lensert.com/fullchain.pem'),
-        ca: fs.readFileSync('/etc/letsencrypt/live/www.lensert.com/chain.pem')
+module.exports = {
+    servers: function () {
+        let servers = []
+        servers.push({
+            server: require('http').createServer(app.callback()),
+            port: process.env.PORT || '3000'
+        })
+        if (fs.existsSync('/etc/letsencrypt/live/www.lensert.com/')) {
+            let options = {
+                key: fs.readFileSync('/etc/letsencrypt/live/www.lensert.com/privkey.pem'),
+                cert: fs.readFileSync('/etc/letsencrypt/live/www.lensert.com/fullchain.pem'),
+                ca: fs.readFileSync('/etc/letsencrypt/live/www.lensert.com/chain.pem')
+            }
+            servers.push({
+                server: require('https').createServer(options, app.callback()),
+                port: 443
+            })
+        }
+        return servers
+    },
+    isSecure: function () {
+        return fs.existsSync('/etc/letsencrypt/live/www.lensert.com/')
     }
-    server = require('https').createServer(options, app.callback())
-} else {
-    server = require('http').createServer(app.callback())
-}
 
-module.exports = server
+}
